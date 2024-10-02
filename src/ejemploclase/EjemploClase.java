7b9d09e14a2a7c76c4b78b68aea1d3d19113b643
osvaldo010 <estradaosvaldo150@gmail.com>
2024-10-02 10:56:38 -0600

    Se agregó un ciclo al método principal

--- src/ejemploclase/EjemploClase.java
+++ src/ejemploclase/EjemploClase.java
@@ -16,7 +16,10 @@ public class EjemploClase
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("hola");
+        }
     }
     
 }
